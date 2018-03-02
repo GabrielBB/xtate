58acb40a4b39328d7a64354eae8385549425364f
GabrielBB03 <gabrielbb0306@gmail.com>
2018-03-02 06:37:39 -0400

    Making the example code more simple

--- example/src/App.js
+++ example/src/App.js
@@ -6,32 +6,19 @@ class App extends Component {
   // The global application state comes from this.props.global and normal parameters are in this.props.local
   addNewArticle = () => {
     let articles = this.props.global.articles;
-    let lastId = articles[articles.length - 1].id + 1;
-    store.dispatch('SAVE_ARTICLE', { id: lastId, text: "Article" })
-  }
 
-  removeLastArticle = () => {
-    if (this.props.global.articles.length > 0) {
-      store.dispatch('DELETE_ARTICLE', this.props.global.articles.length - 1)
-    }
-
+    store.dispatch('SAVE_ARTICLE', { id: 2, text: "Article" })
   }
 
   render() {
-
-    const articles = this.props.global.articles.length === 0 ?
-      <span>No Articles to show</span> :
-      <ul>{this.props.global.articles.map(a => <li key={a.id}>{a.text + ' ' + a.id}</li>)}</ul>;
-
     return (
       <div>
         <button onClick={this.addNewArticle}>Add Article</button>
-        <button onClick={this.removeLastArticle}>Remove Last Article</button>
 
-        {articles}
+        <ul>{this.props.global.articles.map(a => <li key={a.id}>{a.text + ' ' + a.id}</li>)}</ul>
       </div>
 
-    );
+    )
   }
 }
 
--- example/src/index.js
+++ example/src/index.js
@@ -2,5 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-// NO NEED TO ADD A <PROVIDER> </PROVIDER> ! Render your root component as usual
+// No need to wrap your app component around a <Provider> ! Render as usual
 ReactDOM.render(<App />, document.getElementById('root'));
