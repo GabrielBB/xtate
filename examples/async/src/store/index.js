2443e74b26c93aeea036e414513e18d6eb036dc8
GabrielBB03 <gabrielbb0306@gmail.com>
2018-03-03 01:38:21 -0400

    Updating async example code

--- examples/async/src/store/index.js
+++ examples/async/src/store/index.js
@@ -1,7 +1,7 @@
-import { Xtate } from 'xtate';
+import Xtate from 'xtate';
 import axios from 'axios';
 
-const store = new Xtate({}, true);
+const store = new Xtate();
 
 store.actionAsync('UPDATE_DOG_IMAGE', async function (xtate) {
   return await axios.get('https://dog.ceo/api/breeds/image/random');
